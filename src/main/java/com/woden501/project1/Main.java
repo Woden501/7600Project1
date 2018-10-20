9e4dbd280af3e7d8b81d27e095df9d5e9bf5f42a
Colby Snedeker <woden501@gmail.com>
2018-10-20 04:25:02 -0400

    Maven fixes to build and use JAR

--- src/main/java/com/woden501/project1/Main.java
+++ src/main/java/com/woden501/project1/Main.java
@@ -1,13 +1,13 @@
 package com.woden501.project1;
 
 import java.io.IOException;
+import java.net.URI;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,17 +19,15 @@ public class Main {
 
 		// Read the messages file into a string
 		String messageFileString = "";
-
 		try {
-			URL messageFileUrl = ClassLoader.getSystemResource("english_big.txt");
-			Path messageFilePath = Path.of(messageFileUrl.toURI());
+			URI messageFileUri = URI.create(args[0]);
+			Path messageFilePath = Path.of(messageFileUri);
 			messageFileString = Files.readString(messageFilePath, StandardCharsets.ISO_8859_1);
 		} catch (Exception e) {
 			System.out.println("Unable to parse SMS file. Exception: " + e.toString());
 			System.out.println("Exiting..");
+			System.exit(1);
 		}
-
-		System.out.println(messageFileString);
 
 		// Create message object for each message in string
 		String[] lines = messageFileString.split("\n");
@@ -41,13 +39,8 @@ public class Main {
 			Message message = new Message(line.substring(0, lastComma), line.substring(lastComma + 1));
 			messages.add(message);
 		}
-
-		int i = 1;
-		for (Message message : messages) {
-			System.out.println(i + ": " + message.getMessageContents() + ", " + message.getMessageType());
-			i++;
-		}
-
+		
+		// Iterate through all of the messages extracting the four features we want
 		for (Message message : messages) {
 			// Count the number of characters in each message
 			countCharacters(message);
@@ -62,12 +55,6 @@ public class Main {
 			findWordFrequency(message);
 		}
 
-		i = 1;
-		for (Message message : messages) {
-			System.out.println(i + ": " + message.toString());
-			i++;
-		}
-
 		// Write counts to file in ARFF format
 		// Start by generating string
 		String arffOutput = "@RELATION spam\n";
@@ -77,23 +64,29 @@ public class Main {
 		arffOutput += "@ATTRIBUTE frequencycount NUMERIC\n";
 		arffOutput += "@ATTRIBUTE class {ham, spam}\n";
 		arffOutput += "\n@DATA\n";
+		// Then call the overridden toString method on each Message to print out the appropriate
+		// format for the ARFF file
 		for (Message message : messages) {
 			arffOutput += message.toString() + "\n";
 		}
 		
-		// Write string to disk
+		// Write the ARFF string to disk
 		try {
 			Files.writeString(Path.of("project.arff"), arffOutput);
 		} catch (IOException e) {
 			System.out.println("Unable to write ARFF output to disk. Exception: " + e.getMessage());
 			System.exit(1);
 		}
+		
+		System.out.println("ARFF file produced...");
 	}
 
+	// Takes a Message object and counts the number of characters in its contents string
 	private static void countCharacters(Message message) {
 		message.setNumChars(message.getMessageContents().length());
 	}
 
+	// Takes a Message object and counts the number of currency symbols in its contents string
 	private static void countCurrencySymbols(Message message) {
 		int currencySymbolCount = 0;
 		// Look at every character in the message, and check if it is a currency symbol
@@ -105,16 +98,23 @@ public class Main {
 		message.setNumCurrencySymbols(currencySymbolCount);
 	}
 
+	// Takes a Message object and counts the number of numeric strings in its contents string
 	private static void countNumericStrings(Message message) {
 		int numNumericStrings = 0;
+		
+		// Regex pattern that matches all numeric strings of length 1 or more
 		Pattern p = Pattern.compile("-?\\d+");
 		Matcher m = p.matcher(message.getMessageContents());
+		
+		// Repeatedly search for matches to the regex pattern, and each time one is found
+		// we add one to our count of numeric strings.
 		while (m.find()) {
 			numNumericStrings++;
 		}
 		message.setNumNumericStrings(numNumericStrings);
 	}
 
+	// Takes a Message object and finds the count of the most frequently used word in its contents string
 	private static void findWordFrequency(Message message) {
 		// Remove non-whitespace and non-apostrophe puncutation
 		String noPuncationString = message.getMessageContents().replaceAll("[^A-Za-z' ]+", "");
@@ -129,15 +129,19 @@ public class Main {
 		for (String word : words) {
 			Integer currentCount = wordCount.get(word);
 			
+			// If the word isn't in the hashmap yet then add it and set the count to 1
 			if (currentCount == null) {
 				currentCount = 1;
 				wordCount.put(word, currentCount);
 			}
+			// The word is in the hashmap, so we just add 1 to the count
 			else {
 				currentCount++;
 				wordCount.put(word, currentCount);
 			}
 			
+			// Check to see if this new current count is greater than the highest count so far
+			// If so set this as the new highest count
 			if (currentCount > highestCount) {
 				highestCount = currentCount;
 			}
